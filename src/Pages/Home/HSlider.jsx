970023602c788276df9ab374937bc9a87e4d7ca4
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-03 23:02:49 +0600

    slider image has uploaded

---
+++ src/Pages/Home/HSlider.jsx
@@ -0,0 +1,12 @@
+import slide1 from '../../assets/home/slide1.jpg';
+import slide2 from '../../assets/home/slide2.jpg';
+import slide3 from '../../assets/home/slide3.jpg';
+const HSlider = () => {
+    return (
+        <>
+            
+        </>
+    );
+};
+
+export default HSlider;

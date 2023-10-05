04bf661969b9668fe882b69f61c36ae43fd56b7c
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-05 18:20:10 +0600

    login and register page html responsive design has completed

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,12 +1,62 @@
+import {Link} from "react-router-dom";
+
 
 const Login = () => {
-    return (
-        <>
-            <div>
-                <h2 className="text-4xl text-center">Login page</h2>
-            </div>
-        </>
-    );
+  return (
+    <>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left w-1/2">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <img src="https://img.freepik.com/free-vector/mobile-login-concept-illustration_114360-135.jpg?size=626&ext=jpg&ga=GA1.1.28436747.1695030037&semt=sph" />
+          </div>
+          <div className=" w-1/2 card flex-shrink-0  max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <Link to="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </Link>
+                  <Link to="/register" className="label-text-alt link link-hover">
+                    new to here? please Register
+                  </Link>
+                  
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Login;
--- src/Pages/Register/Register.jsx
+++ src/Pages/Register/Register.jsx
@@ -1,13 +1,93 @@
+import { Link } from "react-router-dom";
 
 const Register = () => {
-    return (
-        <>
-        <div>
-            <h2 className="text-4xl text-center">Register page</h2>
+  return (
+    <>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left w-1/2">
+            <h1 className="text-5xl font-bold">Register now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <img src="https://img.freepik.com/free-vector/secure-login-concept-illustration_114360-4582.jpg?size=626&ext=jpg&ga=GA1.1.28436747.1695030037&semt=sph" alt="" />
+          </div>
+          <div className="w-1/2 card flex-shrink-0  max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  type="text"
+                  placeholder="Name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Phone</span>
+                </label>
+                <input
+                  type="tel"
+                  placeholder="Phone"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo URL</span>
+                </label>
+                <input
+                  type="url"
+                  placeholder="Photo URL"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <Link to="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </Link>
+                  <Link to="/login" className="label-text-alt link link-hover">
+                    already have an account? Login
+                  </Link>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Register Now</button>
+              </div>
+            </form>
+          </div>
         </div>
-            
-        </>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Register;

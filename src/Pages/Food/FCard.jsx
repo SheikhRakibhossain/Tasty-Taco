8aa746585b2635590bc7f352dda34972fc947203
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-06 19:56:13 +0600

    added to cart page alert popup has added

--- src/Pages/Food/FCard.jsx
+++ src/Pages/Food/FCard.jsx
@@ -1,8 +1,51 @@
+import { useContext } from "react";
+import { AuthContext } from "../../AuthProvider/AuthProvider";
+import Swal from 'sweetalert2'
+import { useNavigate } from "react-router-dom";
+
 const FCard = ({ item }) => {
   const { image, price, name, recipe } = item;
 
+  const { user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
   const handleAddToCart = (CartFood) => {
     console.log(CartFood);
+    if (user) {
+      // console.log(user)
+      fetch("http://localhost:5000/carts")
+        .then((res) => res.json())
+        .then((data) => {
+          if (data.insertedId) {
+            Swal.fire({
+              position: "top-end",
+              icon: "success",
+              title: "Product has added to the cart",
+              showConfirmButton: false,
+              timer: 1000,
+            });
+          }
+        });
+    }
+    else {
+      Swal.fire({
+        title: 'Please Login before Buying',
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Login'
+      }).then((result) => {
+        if (result.isConfirmed) {
+          Swal.fire(
+            'Re-directed to login page!',
+            'click ok and login for added the food',
+            'success',
+            navigate('/login')
+          )
+        }
+      })
+    }
   };
 
   return (

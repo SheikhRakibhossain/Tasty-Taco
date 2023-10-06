6db2908b08ff7b8640b60e339377ca9cc12fd0a9
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-07 01:37:13 +0600

    tanstack quwery have added and useCart hook has created for data passing for add to cart page

---
+++ src/Hooks/useCart.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react";
+import { AuthContext } from "../AuthProvider/AuthProvider";
+import { useQuery } from "@tanstack/react-query";
+
+const useCart = () => {
+  const { user } = useContext(AuthContext);
+
+  const { isLoading, refetch, data: cart = [] } = useQuery({
+    queryKey: ['carts', user?.email],
+    queryFn: async () =>{
+      const res = await fetch(`http://localhost:5000/carts?email=${user?.email}`)
+        return res.json();
+    }
+  });
+  return [cart, isLoading, refetch];
+};
+
+export default useCart;

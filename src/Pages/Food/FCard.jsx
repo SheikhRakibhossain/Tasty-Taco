23c6afa5a26d39b6d71ad1e93bcb574abd72c25f
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-06 18:39:41 +0600

    button add to cart has added for running the function

--- src/Pages/Food/FCard.jsx
+++ src/Pages/Food/FCard.jsx
@@ -1,23 +1,29 @@
 const FCard = ({ item }) => {
   const { image, price, name, recipe } = item;
 
+  const handleAddToCart = (CartFood) => {
+    console.log(CartFood);
+  };
+
   return (
     <>
       <div className="card w-auto  bg-base-100 shadow-xl">
         <figure className="border-0 relative">
-          <img
-            src={image}
-            alt="Shoes"
-            className="rounded-xl"
-          />
-         
+          <img src={image} alt="Shoes" className="rounded-xl" />
         </figure>
-        <span className="absolute top-2 right-2 px-4 py-2 bg-[#00000085] text-white">${price}</span>
+        <span className="absolute top-2 right-2 px-4 py-2 bg-[#00000085] text-white">
+          ${price}
+        </span>
         <div className="card-body items-center text-center">
           <h2 className="card-title">{name}</h2>
           <p>{recipe}</p>
           <div className="card-actions">
-            <button className="btn btn-primary btn-sm">Buy Now</button>
+            <button
+              onClick={() => handleAddToCart(item)}
+              className="btn btn-primary btn-sm"
+            >
+              Buy Now
+            </button>
           </div>
         </div>
       </div>

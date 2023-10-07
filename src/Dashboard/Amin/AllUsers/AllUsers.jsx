3693696a64dbfebfca11ec13bbf84a9522c298c8
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-08 04:12:20 +0600

    successfully admin user function have added

--- src/Dashboard/Amin/AllUsers/AllUsers.jsx
+++ src/Dashboard/Amin/AllUsers/AllUsers.jsx
@@ -1,39 +1,58 @@
 import { useQuery } from "@tanstack/react-query";
 import { Helmet } from "react-helmet-async";
 import { FaTrash, FaUserAltSlash } from "react-icons/fa";
+import Swal from "sweetalert2";
 
 const AllUsers = () => {
   const { data: users = [], refetch } = useQuery(["users"], async () => {
-    const res = await fetch("http://localhost:5000/users")
+    const res = await fetch("http://localhost:5000/users");
     return res.json();
   });
 
-const handleDelete =(user)=>{
-    console.log('norml user',user)
+  const handleDelete = (user) => {
+    console.log("norml user", user);
     refetch();
+  };
+  const handleUserRole = (user) => {
+    // console.log("hadle role click", user);
+    fetch(`http://localhost:5000/users/admin/${user._id}`,{
+        method:"PATCH",
+    })
+    .then(res => res.json())
+    .then(data =>{
+        refetch();
+        if(data.modifiedCount){
+            Swal.fire({
+                position: 'top-end',
+                icon: 'success',
+                title: `${user.name} is now a admin`,
+                showConfirmButton: false,
+                timer: 1500
+              })
+        }
     
-
-}
-const handleUserRole =(user)=>{
-    console.log('hadle role click', user)
-    refetch();
-}
+    })
+    .catch(error =>console.log("going to make admin error",error))
+    
+  };
   return (
     <>
-      
       <Helmet>
         <title>Testy Taco | All users</title>
       </Helmet>
       <div className="py-6">
         <p className="text-center italic">----- All Users -----</p>
-        <h3 className="text-center text-4xl uppercase font-semibold">Hers{"'"}s Your website all users</h3>
-
+        <h3 className="text-center text-4xl uppercase font-semibold">
+          Hers{"'"}s Your website all users
+        </h3>
       </div>
 
       {/* table all */}
       <div className="overflow-x-auto min-w-full">
         <div className="flex justify-between bg-purple-400  py-4 px-4">
-          <h2 className="text-2xl font-semibold text-white">Total Users: {users.length}</h2>
+          <h2 className="text-2xl font-semibold text-white">
+            Total Users: {users.length}
+          </h2>
           <h2 className="text-2xl font-semibold text-white">Total </h2>
           <button className="btn btn-primary btn-sm">Action</button>
         </div>
@@ -52,14 +71,19 @@ const handleUserRole =(user)=>{
             {/* row 1 */}
             {users.map((user, index) => (
               <tr key={user._id}>
-                <td>{index+1}</td>
+                <td>{index + 1}</td>
                 <td>{user.name}</td>
                 <td>{user.email}</td>
-                {
-                    user.role ==="admin" ?"Admin":<span onClick={() => handleUserRole(user)}
-                    className="hover:cursor-pointer "> <FaUserAltSlash className="border p-2 text-4xl rounded bg-red-500 hover:text-yellow-400" />
-                    </span>
-                }
+                <td>
+                  {user.role === "admin" ? (
+                    "Admin"
+                  ) : (
+                    <FaUserAltSlash
+                      onClick={() => handleUserRole(user)}
+                      className="border p-2 text-4xl hover:cursor-pointer  rounded bg-gray-900-500 hover:text-primary"
+                    />
+                  )}
+                </td>
                 <td
                   onClick={() => handleDelete(user)}
                   className="hover:cursor-pointer "
@@ -71,7 +95,6 @@ const handleUserRole =(user)=>{
           </tbody>
         </table>
       </div>
- 
     </>
   );
 };

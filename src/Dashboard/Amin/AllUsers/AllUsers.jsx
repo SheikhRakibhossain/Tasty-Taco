794ba17a1fc7708c9d5bc21fcda1a152204c699d
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-08 03:17:18 +0600

    user data show tanstack query has crated

--- src/Dashboard/Amin/AllUsers/AllUsers.jsx
+++ src/Dashboard/Amin/AllUsers/AllUsers.jsx
@@ -1,10 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
 
 const AllUsers = () => {
-    return (
-        <>
-            <h2>I am all users page</h2>
-        </>
-    );
+  const { data: users = [], refetch } = useQuery(["users"], async () => {
+    const res = await fetch("http://localhost:5000/users")
+    return res.json();
+  });
+
+  return (
+    <>
+      <h2>I am all users page</h2>
+      <h2>All users {users.length}</h2>
+    </>
+  );
 };
 
 export default AllUsers;

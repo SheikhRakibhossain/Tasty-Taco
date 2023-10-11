f8884a7ebe350c8a9106aeb0d38659391e7f9e79
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-11 23:56:00 +0600

    about page 2nd section has created

--- src/Pages/About/About.jsx
+++ src/Pages/About/About.jsx
@@ -14,6 +14,17 @@ const About = () => {
           </div>
         </div>
       </div>
+      {/* brower added */}
+      <div className="mockup-browser border border-base-300">
+        <div className="mockup-browser-toolbar">
+          <div className="input border border-base-300">
+            https://rakib.com
+          </div>
+        </div>
+        <div className="flex justify-center px-4 py-16 border-t border-base-300">
+          Hello!
+        </div>
+      </div>
     </>
   );
 };

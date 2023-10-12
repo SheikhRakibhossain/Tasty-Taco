1f091734fe81d2e35122412b790589b0153d3f87
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-12 23:06:00 +0600

    image url api has added for add item image on the server site

--- src/Dashboard/Amin/AddItem/AddItem.jsx
+++ src/Dashboard/Amin/AddItem/AddItem.jsx
@@ -1,65 +1,84 @@
+import { Helmet } from "react-helmet-async";
 import { useForm } from "react-hook-form";
 
 const AddItem = () => {
+    const imageApi = import.meta.env.VITE_IMAGE_API;
+    console.log(imageApi);
+    const image_hosting_url = `https://api.imgbb.com/1/upload?expiration=600&key=${imageApi}`
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
+//   data submit form
   const onSubmit = (data) => {
+
     console.log(data);
   };
   console.log(errors);
   return (
     <>
-      <h2>i am add item page</h2>
-      <div>
+        <Helmet>
+        <title>Testy Taco | Add Item</title>
+      </Helmet>
+      <div className="py-6">
+        <p className="text-center italic">----- Add Items Here -----</p>
+        <h3 className="text-center text-4xl uppercase font-semibold">
+          Hers{"'"}s is our menu adding form.
+        </h3>
+      </div>
+      <div className="py-8">
+        <h2 className="text-4xl text-center py-4">Add Items</h2>
+        <div className="divider"></div>
         <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
-          <div className="form-control">
-            <input
-              className="input input-bordered"
-              type="text"
-              placeholder="Recipe Name"
-              {...register("Recipe Name", { required: true })}
-            />
-          </div>
-          <div className="form-control">
-            <select
-              {...register("Category", { required: true })}
-              className="input input-bordered"
-            >
-              <option value="salad">salad</option>
-              <option value=" dessert"> dessert</option>
-              <option value=" drinks"> drinks</option>
-              <option value=" pizza"> pizza</option>
-              <option value=" soup"> soup</option>
-            </select>
-          </div>
-          <div className="form-control">
-            <input
-              className="input input-bordered"
-              type="number"
-              placeholder="Price"
-              {...register("Price", { required: true })}
-            />
-          </div>
+          <div className="grid md:grid-cols-2 gap-4">
+            <div className="form-control">
+              <input
+                className="input input-bordered"
+                type="text"
+                placeholder="Recipe Name"
+                {...register("Recipe Name", { required: true })}
+              />
+            </div>
+            <div className="form-control">
+              <select
+                {...register("Category", { required: true })}
+                className="input input-bordered"
+              >
+                <option value="salad">salad</option>
+                <option value=" dessert"> dessert</option>
+                <option value=" drinks"> drinks</option>
+                <option value=" pizza"> pizza</option>
+                <option value=" soup"> soup</option>
+              </select>
+            </div>
+            <div className="form-control">
+              <input
+                className="input input-bordered"
+                type="number"
+                placeholder="Price $"
+                {...register("Price", { required: true })}
+              />
+            </div>
 
-          <div className="form-contorl">
-            <textarea
-              className="input input-bordered"
-              {...register("Recipe Details", { required: true })}
-            />
-          </div>
-          <div className="form-control">
-            <input
-              className="input input-bordered"
-              type="file"
-              placeholder="Recipe Image"
-              {...register("Recipe Image", { required: true })}
-            />
-          </div>
+            <div className="form-contorl w-full">
+              <textarea
+                className="textarea textarea-primary w-full"
+                {...register("RecipeDetails", { required: true })}
+                placeholder="Recipe Details"
+              />
+            </div>
+            <div className="form-control">
+              <input
+                className="input input-bordered"
+                type="file"
+                placeholder="Recipe Image"
+                {...register("image", { required: true })}
+              />
+            </div>
 
-          <input type="submit" value="Add Item" className="btn btn-primary" />
+            <input type="submit" value="Add Item" className="btn btn-primary" />
+          </div>{" "}
         </form>
       </div>
     </>

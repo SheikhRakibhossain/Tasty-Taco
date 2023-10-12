950ec6555ab455ad7c6d26666204cbd92b9aedae
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-12 16:03:45 +0600

    add item form has desiged

--- src/Dashboard/Amin/AddItem/AddItem.jsx
+++ src/Dashboard/Amin/AddItem/AddItem.jsx
@@ -6,39 +6,57 @@ const AddItem = () => {
     handleSubmit,
     formState: { errors },
   } = useForm();
-  const onSubmit = (data) => console.log(data);
+  const onSubmit = (data) => {
+    console.log(data);
+  };
   console.log(errors);
   return (
     <>
       <h2>i am add item page</h2>
-
       <div>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            type="text"
-            placeholder="Recipe Name"
-            {...register("Recipe Name", { required: true })}
-          />
-          <select {...register("Category", { required: true })}>
-            <option value="salad">salad</option>
-            <option value=" dessert"> dessert</option>
-            <option value=" drinks"> drinks</option>
-            <option value=" pizza"> pizza</option>
-            <option value=" soup"> soup</option>
-          </select>
-          <input
-            type="number"
-            placeholder="Price"
-            {...register("Price", { required: true })}
-          />
-          <textarea {...register("Recipe Details", { required: true })} />
-          <input
-            type="file"
-            placeholder="Recipe Image"
-            {...register("Recipe Image", { required: true })}
-          />
+          <div className="form-control">
+            <input
+              className="input input-bordered"
+              type="text"
+              placeholder="Recipe Name"
+              {...register("Recipe Name", { required: true })}
+            />
+          </div>
+          <div className="form-control">
+            <select {...register("Category", { required: true })}>
+              <option value="salad">salad</option>
+              <option value=" dessert"> dessert</option>
+              <option value=" drinks"> drinks</option>
+              <option value=" pizza"> pizza</option>
+              <option value=" soup"> soup</option>
+            </select>
+          </div>
+          <div className="form-control">
+            <input
+              className="input input-bordered"
+              type="number"
+              placeholder="Price"
+              {...register("Price", { required: true })}
+            />
+          </div>
 
-          <input type="submit" />
+          <div className="form-contorl">
+            <textarea
+              className="input input-bordered"
+              {...register("Recipe Details", { required: true })}
+            />
+          </div>
+          <div className="form-control">
+            <input
+              className="input input-bordered"
+              type="file"
+              placeholder="Recipe Image"
+              {...register("Recipe Image", { required: true })}
+            />
+          </div>
+
+          <input type="submit" value="Add Item" className="btn btn-primary"/>
         </form>
       </div>
     </>

e6f2e082c4030837567fcd93b07ce3a8c50fcc82
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-13 23:59:09 +0600

    tanstak query has added for add menu item

--- src/Dashboard/Amin/ManageItem/ManageItem.jsx
+++ src/Dashboard/Amin/ManageItem/ManageItem.jsx
@@ -1,10 +1,23 @@
+import { Helmet } from "react-helmet-async";
 
 const ManageItem = () => {
-    return (
-        <div>
-            Manage item page
-        </div>
-    );
+  return (
+    <>
+      <Helmet>
+        <title>Testy Taco | Mange item</title>
+      </Helmet>
+      <div className="py-6">
+        <p className="text-center italic">----- Manage Item -----</p>
+        <h3 className="text-center text-4xl uppercase font-semibold">
+          Manage{"'"}s Your Menu Items
+        </h3>
+      </div>
+
+      <div>
+        
+      </div>
+    </>
+  );
 };
 
 export default ManageItem;

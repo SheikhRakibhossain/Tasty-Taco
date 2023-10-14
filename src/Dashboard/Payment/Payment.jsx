885689df3ec8c252b6247fdc02893969360c67e0
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-14 19:53:41 +0600

    paymnet page header and  helmet added

--- src/Dashboard/Payment/Payment.jsx
+++ src/Dashboard/Payment/Payment.jsx
@@ -1,7 +1,16 @@
+import { Helmet } from "react-helmet-async";
 
 const Payment = () => {
     return (
         <>
+        <Helmet>
+        <title>Testy Taco |Payment</title>
+      </Helmet>
+      <div className="py-6">
+        <p className="text-center italic">----- Paymnet Proceccing -----</p>
+        <h3 className="text-center text-4xl uppercase font-semibold">
+payment here        </h3>
+      </div>
         <div>
             <h2>I am payment page</h2>
         </div>

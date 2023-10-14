d767b7e496426e0b00aa4e2b95f876938b6615e1
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-14 21:50:55 +0600

    stripe payment card seystem has intregrated

--- src/Dashboard/Payment/Payment.jsx
+++ src/Dashboard/Payment/Payment.jsx
@@ -1,22 +1,34 @@
 import { Helmet } from "react-helmet-async";
+import CheckOut from "./CheckOut";
+import { loadStripe} from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js";
+
 
 const Payment = () => {
-    return (
-        <>
-        <Helmet>
+  const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_SECRET_PUBLISHABLE_KEY);
+
+  return (
+    <>
+      <Helmet>
         <title>Testy Taco |Payment</title>
       </Helmet>
       <div className="py-6">
         <p className="text-center italic">----- Paymnet Proceccing -----</p>
         <h3 className="text-center text-4xl uppercase font-semibold">
-payment here        </h3>
+          payment here{" "}
+        </h3>
       </div>
-        <div>
-            <h2>I am payment page</h2>
-        </div>
-            
-        </>
-    );
+      <div>
+        <h2>I am payment page</h2>
+      </div>
+      {/* payment checkout form */}
+      <div className="w-full">
+        <Elements stripe={stripePromise}>
+          <CheckOut></CheckOut>
+        </Elements>
+      </div>
+    </>
+  );
 };
 
 export default Payment;

681be9acc380ece738265b0ff25e81ff772b8cb5
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-17 00:00:02 +0600

    made color like before

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -43,7 +43,7 @@ const Dashboard = () => {
             aria-label="close sidebar"
             className="drawer-overlay"
           ></label>
-          <ul className="menu p-4 w-80 min-h-full bg-base-100 text-base-content">
+          <ul className="menu p-4 w-80 min-h-full bg-base-300 text-base-content">
             {/* Sidebar content here */}
             {isAdmin ? (
               <>

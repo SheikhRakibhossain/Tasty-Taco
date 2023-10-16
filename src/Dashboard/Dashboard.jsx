8c657e897edaaf117820e53087a20d3b4105dff5
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-16 23:58:30 +0600

    dashboard design has completed

--- src/Dashboard/Dashboard.jsx
+++ src/Dashboard/Dashboard.jsx
@@ -24,7 +24,7 @@ const Dashboard = () => {
   const [isAdmin] = useAdmin();
   return (
     <>
-      <div className="drawer lg:drawer-open">
+      <div className="drawer lg:drawer-open -m-4">
         <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col items-center justify-normal">
           {/* Page content here */}
@@ -43,7 +43,7 @@ const Dashboard = () => {
             aria-label="close sidebar"
             className="drawer-overlay"
           ></label>
-          <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content">
+          <ul className="menu p-4 w-80 min-h-full bg-base-100 text-base-content">
             {/* Sidebar content here */}
             {isAdmin ? (
               <>

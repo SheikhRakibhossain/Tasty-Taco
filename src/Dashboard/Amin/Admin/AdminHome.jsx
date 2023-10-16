02079a4a949a14536abd39aa4bd85e96d57397d2
SheikhRakibhossain <isheikhrakibhossain@gmail.com>
2023-10-16 15:35:29 +0600

    admin data has sent completed function is working with mongoos setup

--- src/Dashboard/Amin/Admin/AdminHome.jsx
+++ src/Dashboard/Amin/Admin/AdminHome.jsx
@@ -36,8 +36,8 @@ const AdminHome = () => {
               ></path>
             </svg>
           </div>
-          <div className="stat-title">Total Likes</div>
-          <div className="stat-value text-primary">25.6K</div>
+          <div className="stat-title">Total Revenue</div>
+          <div className="stat-value text-primary">${stats.revenue}K</div>
           <div className="stat-desc">21% more than last month</div>
         </div>
 
@@ -57,34 +57,30 @@ const AdminHome = () => {
               ></path>
             </svg>
           </div>
-          <div className="stat-title">Page Views</div>
-          <div className="stat-value text-secondary">2.6M</div>
+          <div className="stat-title">Total Products</div>
+          <div className="stat-value text-secondary">{stats.products}</div>
           <div className="stat-desc">21% more than last month</div>
         </div>
 
         <div className="stat">
           <div className="stat-figure text-secondary">
             <div className="avatar online">
-              <div className="w-16 rounded-full">
-                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-              </div>
+              
             </div>
           </div>
-          <div className="stat-value">86%</div>
-          <div className="stat-title">Tasks done</div>
-          <div className="stat-desc text-secondary">31 tasks remaining</div>
+          <div className="stat-title">Total Users</div>
+          <div className="stat-value">{stats.users}</div>
+          <div className="stat-desc text-accent-content">31 tasks remaining</div>
         </div>
         <div className="stat">
           <div className="stat-figure text-secondary">
             <div className="avatar online">
-              <div className="w-16 rounded-full">
-                <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-              </div>
+              
             </div>
           </div>
-          <div className="stat-value">86%</div>
-          <div className="stat-title">Tasks done</div>
-          <div className="stat-desc text-secondary">31 tasks remaining</div>
+          <div className="stat-title">Total Orders</div>
+          <div className="stat-value text-teal-500">{stats.orders}</div>
+          <div className="stat-desc text-secondary">12 is pending</div>
         </div>
       </div>
     </>
